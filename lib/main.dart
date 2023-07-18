d4e91907ec58c460ac2b74cd71aedb722d7c77c3
aneebshinwari <aneeb.shinwari10@gmail.com>
2023-07-18 11:55:40 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomNavigation(),
+    );
+  }
+}
+
+class BottomNavigation extends StatefulWidget {
+  @override
+  _BottomNavigationState createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent[200],
+        title: Center(child: Text('Bottom Nevigation bar')),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+
+          BottomNavigationBarItem(
+            backgroundColor: Colors.deepPurpleAccent,
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.deepPurpleAccent,
+
+            icon: Icon(
+              Icons.search,
+            ),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.deepPurpleAccent,
+
+            icon: Icon(
+              Icons.favorite,
+            ),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.deepPurpleAccent,
+
+            icon: Icon(
+              Icons.person,
+            ),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
